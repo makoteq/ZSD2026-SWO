d88a41476c52235abbb47544aa335ccee5ec12ac
Maciej <42750440+makoteq@users.noreply.github.com>
2026-03-21 16:46:58 +0100

    Enhance model prediction with TPU selection
    Added device selection for TPU in prediction.

--- hardware/test.py
+++ hardware/test.py
@@ -4,4 +4,7 @@ from ultralytics import YOLO
 model = YOLO("./yolo26n_full_integer_quant.tflite")  # Load an official model or custom model
 
 # Run Prediction
-model.predict("./sample.jpg")
+model.predict("./sample.jpg")  # Inference defaults to the first TPU
+
+model.predict("./sample.jpg", device="tpu:0")  # Select the first TPU
+
