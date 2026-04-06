8c907e62be393ac27749c2813c6a441165f7679e
Cingciong <igor.w.maciejak@gmail.com>
2026-04-06 17:53:16 +0200

    adding autamtic camera roll calculation + working  on lane detct via camera

--- Detection_dev/radar_reading/mapping.py
+++ Detection_dev/radar_reading/mapping.py
@@ -8,7 +8,7 @@ from matplotlib.widgets import Slider
 from sklearn.linear_model import LinearRegression
 import os
 
-RELATIVE_CSV_PATH: str = "../../data/carla/scenario5_empty_road/radar_points_world.csv"
+RELATIVE_CSV_PATH: str = "../../data\\batch2\\batch2\\scenario1_speeding_2cars\\run_003\\radar_points_world.csv"
 
 COLUMN_X: str = "x_sensor"
 COLUMN_Y: str = "y_sensor"
@@ -16,19 +16,20 @@ COLUMN_Z: str = "z_sensor"
 
 SENSOR_PITCH_DEG: float = 0.0
 SENSOR_YAW_DEG: float = 0.0
-SENSOR_ROLL_DEG: float = 5.0
+SENSOR_ROLL_DEG: float = 0.0
 CAMERA_HEIGHT_OFFSET: float = 6.0
 
-MASK_Z_MIN: float = 0.0
-MASK_Z_MAX: float = 1.0
-MASK_Y_MIN: float = 00.0
-MASK_Y_MAX: float = 200.0
+MASK_Z_MIN: float = 0.15
+MASK_Z_MAX: float = 5.0
+MASK_Y_MIN: float = 72.0
+MASK_Y_MAX: float = 90.0
 
 GRID_RES: float = 0.1
 STREET_Z_THRESHOLD: float = 0.5
 MORPH_ITERATIONS: int = 3
 MIN_CONTOUR_POINTS: int = 15
 INTERPOLATION_FACTOR: int = 2
+SIZE_POINT_PHOTO: int = 700
 
 SIGMOID_STEEPNESS: float = 31.94
 SIGMOID_OFFSET: float = 0.0714
@@ -51,9 +52,44 @@ class Street:
         self.fittedLines: list[np.ndarray] = []
         self.lastUsedOffset: float = SIGMOID_OFFSET
 
+    def calculateRoll(self, closeWindow: float = 20.0, farWindow: float = 20.0, numLowest: int = 10) -> float:
+        if self.dataFrame.empty:
+            return 0.0
+            
+        y: pd.Series = self.dataFrame[COLUMN_Y]
+        
+        yMin: float = y.min()
+        yMax: float = y.max()
+        
+        closeMask = (y >= yMin) & (y <= yMin + closeWindow)
+        farMask = (y >= yMax - farWindow) & (y <= yMax)
+        
+        closePoints = self.dataFrame[closeMask]
+        farPoints = self.dataFrame[farMask]
+        
+        if closePoints.empty or farPoints.empty:
+            return 0.0
+
+        closeLowest = closePoints.nsmallest(numLowest, COLUMN_Z)
+        farLowest = farPoints.nsmallest(numLowest, COLUMN_Z)
+        
+        zCloseMean: float = closeLowest[COLUMN_Z].mean()
+        yCloseMean: float = closeLowest[COLUMN_Y].mean()
+        
+        zFarMean: float = farLowest[COLUMN_Z].mean()
+        yFarMean: float = farLowest[COLUMN_Y].mean()
+        
+        deltaZ: float = zFarMean - zCloseMean
+        deltaY: float = yFarMean - yCloseMean
+        
+        angleRad: float = np.arctan2(deltaZ, deltaY)
+
+        return float(np.degrees(angleRad))
+
     def adjustPoints(self, pitch: float, yaw: float, roll: float, heightOffset: float) -> None:
         pitchRad: float = np.radians(-pitch)
         yawRad: float = np.radians(-yaw)
+        roll = self.calculateRoll()
         rollRad: float = np.radians(-roll)
 
         cosP, sinP = np.cos(pitchRad), np.sin(pitchRad)
@@ -81,7 +117,8 @@ class Street:
             (self.dataFrame["z_corrected"] >= zMin) & 
             (self.dataFrame["z_corrected"] <= zMax) &
             (self.dataFrame["y_corrected"] >= yMin) &
-            (self.dataFrame["y_corrected"] <= yMax)
+            (self.dataFrame["y_corrected"] <= yMax) &
+            (self.dataFrame["radial_velocity"] == 0)
         ].copy()
 
     def interpolateCloud(self, factor: int) -> None:
@@ -150,11 +187,10 @@ class Street:
         self.sliderStp.on_changed(update)
         plt.show()
 
-    def findStreetContours(self, steepness: float = SIGMOID_STEEPNESS, offset: float = SIGMOID_OFFSET) -> None:
+    def findContours(self) -> None:
         if self.dataFrame.empty:
             return
 
-        self.lastUsedOffset = offset
         x, y, z = self.dataFrame["x_corrected"].values, self.dataFrame["y_corrected"].values, self.dataFrame["z_corrected"].values
         xMin, xMax, yMin, yMax = x.min(), x.max(), y.min(), y.max()
         w, h = int((yMax - yMin) / GRID_RES) + 1, int((xMax - xMin) / GRID_RES) + 1
@@ -165,53 +201,56 @@ class Street:
             if hMap[xIdx[i], yIdx[i]] < z[i]:
                 hMap[xIdx[i], yIdx[i]] = z[i]
 
-        enhanced = self._sigmoid(hMap, steepness, offset)
-        normalized = (enhanced * 255).astype(np.uint8)
-        
-        _, binary = cv2.threshold(normalized, int(STREET_Z_THRESHOLD * 255), 255, cv2.THRESH_BINARY)
-        kernel = np.ones((3, 3), np.uint8)
-        binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel, iterations=MORPH_ITERATIONS)
-        
-        rawContours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        self.contours, self.fittedLines = [], []
-        for cnt in rawContours:
-            pts = cnt.reshape(-1, 2)
-            if len(pts) < MIN_CONTOUR_POINTS:
-                continue
-            
-            wX = pts[:, 1] * GRID_RES + xMin
-            wY = pts[:, 0] * GRID_RES + yMin
+  
 
-            model = LinearRegression()
-            X_reg = wX.reshape(-1, 1)
-            model.fit(X_reg, wY)
-            
-            self.contours.append(np.column_stack((wX, wY)))
-            xRange = np.array([wX.min(), wX.max()]).reshape(-1, 1)
-            self.fittedLines.append(np.column_stack((xRange, model.predict(xRange))))
+        fig = plt.figure(frameon=False)
+        fig.set_size_inches(10, 8)
+        ax = plt.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        
+        ax.imshow(hMap, cmap='plasma', origin='lower', extent=[xMin, xMax, yMin, yMax], alpha=0, aspect='auto')
+        ax.scatter(x, y, c=z, s=SIZE_POINT_PHOTO, cmap='plasma', edgecolors='k', linewidth=0)
+        
+        fig.savefig('bird_eye_heatmap.png', dpi=300)
+        # plt.show()
 
-    def visualize(self) -> None:
-        fig = px.scatter_3d(
-            self.dataFrame, x="x_corrected", y="y_corrected", z="z_corrected",
-            color="z_corrected", color_continuous_scale="Turbo",
-            title=PLOT_TITLE, opacity=OPACITY_LEVEL
-        )
-        for i, line in enumerate(self.fittedLines):
-            fig.add_trace(go.Scatter3d(
-                x=line[:, 0], y=line[:, 1], z=[self.lastUsedOffset, self.lastUsedOffset],
-                mode='lines', line=dict(color='yellow', width=10), name=f'Edge {i}'
-            ))
-        fig.update_layout(scene=dict(aspectmode="data"), margin=dict(l=0, r=0, b=0, t=40))
-        fig.show(renderer=RENDERER_TYPE)
+
+    def enhanceImage(self) -> None:
+        img = cv2.imread('bird_eye_heatmap.png')
+        if img is None:
+            print("Image 'bird_eye_heatmap.png' not found.")
+            return
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        fig, ax = plt.subplots(figsize=(10, 8))
+        
+        kernel = 101
+        sigma = 50
+        contrast = 1.75
+        
+        if kernel > 1:
+            blurred = cv2.GaussianBlur(img, (kernel, kernel), sigma)
+        else:
+            blurred = img
+        enhanced = cv2.convertScaleAbs(blurred, alpha=contrast, beta=0)
+        
+        sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        sharpened = cv2.filter2D(enhanced, -1, sharpen_kernel)
+        
+        ax.imshow(sharpened)
+        ax.set_title('Enhanced and Sharpened Image')
+        
+        plt.savefig('enhanced_sharpened.png', dpi=300, bbox_inches='tight')
+        plt.show()
 
 if __name__ == "__main__":
     streetInstance = Street(RELATIVE_CSV_PATH)
     streetInstance.adjustPoints(SENSOR_PITCH_DEG, SENSOR_YAW_DEG, SENSOR_ROLL_DEG, CAMERA_HEIGHT_OFFSET)
     streetInstance.applyMask(MASK_Z_MIN, MASK_Z_MAX, MASK_Y_MIN, MASK_Y_MAX)
-    
-
     streetInstance.interpolateCloud(INTERPOLATION_FACTOR)
-    
-    # streetInstance.findStreetContours()
-    streetInstance.visualize()
+
+    streetInstance.findContours()
+    streetInstance.enhanceImage()
+    # streetInstance.visualize()
